a8d41668df11ecc3191bfe57a9af719b85bca2aa
iraci34 <netinho.iraci@gmail.com>
2021-12-08 17:11:23 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/iracineto/workshopmongo/domain/Post.java
+++ src/main/java/com/iracineto/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.iracineto.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 
 import com.iracineto.workshopmongo.dto.AuthorDTO;
+import com.iracineto.workshopmongo.dto.CommentDTO;
 
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -17,6 +20,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post(){
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -86,4 +99,5 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
